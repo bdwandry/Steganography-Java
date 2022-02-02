f8ced78268e3c4a5569b6dfc2441bf29881012d0
Bryan Wandrych <bdwandry@mtu.edu>
2022-02-02 14:50:01 -0500

    Got Image Writer working from buffered Image

--- src/Encode/Encode.java
+++ src/Encode/Encode.java
@@ -1,11 +1,12 @@
 package Encode;
 
-import javax.imageio.ImageIO;
+import javax.imageio.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
+
+import static java.awt.image.BufferedImage.TYPE_INT_RGB;
 
 public class Encode {
     private String FilePath;
@@ -39,12 +40,35 @@ public class Encode {
         }
 
         //For testing purposes
-        for (int y = 0; y < image.getHeight(); y++) {
-            for (int x = 0; x < image.getWidth(); x++) {
-                System.out.println(ImageData[y][x].getR());
-                System.out.println(ImageData[y][x].getG());
-                System.out.println(ImageData[y][x].getB());
+//        for (int y = 0; y < image.getHeight(); y++) {
+//            for (int x = 0; x < image.getWidth(); x++) {
+//                System.out.println(ImageData[y][x].getR());
+//                System.out.println(ImageData[y][x].getG());
+//                System.out.println(ImageData[y][x].getB());
+//            }
+//        }
+        encodeImageData(image.getHeight(), image.getWidth());
+    }
+
+    private void encodeImageData(int imageHeight, int imageWidth) throws IOException {
+        //Do stuff here later
+
+        WriteOutImage(imageHeight, imageWidth);
+    }
+
+    private void WriteOutImage(int imageHeight, int imageWidth) throws IOException {
+        BufferedImage EncodedImage = new BufferedImage(imageWidth, imageHeight, TYPE_INT_RGB);
+
+        for (int y = 0; y < imageHeight; y++) {
+            for (int x = 0; x < imageWidth; x++) {
+                int RGB = ImageData[y][x].getR();
+                RGB = (RGB << 8) + ImageData[y][x].getG();
+                RGB = (RGB << 8) + ImageData[y][x].getB();
+
+                EncodedImage.setRGB(x, y, RGB);
             }
         }
+
+        ImageIO.write(EncodedImage, "png", new File("test.png"));
     }
 }

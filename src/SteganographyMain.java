0c10d7fb55ad51fe9cb2a8a82c0386e8bdc480ee
Bryan Wandrych <bdwandry@mtu.edu>
2022-02-02 11:36:42 -0500

    Got a main committed

---
+++ src/SteganographyMain.java
@@ -0,0 +1,8 @@
+//This program is a java implementation of steganography of a picture.
+//Its goal is to encrypt/decrypt text into a picture, this program can also be used to decode said image as well.
+
+public class SteganographyMain {
+    public static void main(String[] args) {
+        System.out.println("Hello world for first commit.");
+    }
+}
